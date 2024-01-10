38853de24abd03cd9163541f91eb768b10efdb67
Chhayuth <chhayuth0@gmail.com>
2024-01-10 10:23:03 +0700

    start project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className=" max-w-[2000px] mx-auto">
+      <div className="flex items-center justify-center p-10">
+        <div className=" text-3xl font-semibold">Mini Course</div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Table />
+      </div>
+    </div>
+  );
+}
+
+export default App;
